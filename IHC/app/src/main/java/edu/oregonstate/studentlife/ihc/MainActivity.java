b4f5dbb1d5793e186cc05be6ee09af87b778539b
Omeed Habibelahian <habibelo@oregonstate.edu>
2017-12-11 12:35:52 -0800

    More work on home page. Haven't done much rendering in Android Studio yet, just testing in-browser because it's HTML.

--- IHC/app/src/main/java/edu/oregonstate/studentlife/ihc/MainActivity.java
+++ IHC/app/src/main/java/edu/oregonstate/studentlife/ihc/MainActivity.java
@@ -3,15 +3,21 @@ package edu.oregonstate.studentlife.ihc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    EditText ed1;
+
+    private WebView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView view = new WebView(this);
-        view.getSettings().setJavaScriptEnabled(true);
+        b1 = (Button)findViewById(R.id.button);
+
         view.loadUrl("file:///android_asset/index.html");
         setContentView(view);
     }
