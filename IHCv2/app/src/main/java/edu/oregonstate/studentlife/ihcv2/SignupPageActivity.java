31d8a73482e5957e551c7a9eec43f42f3964b1a2
blazerzero <habibelo@oregonstate.edu>
2017-12-20 15:34:24 -0800

    Filled android manifest. made signuppage java file. added to a bunch of xml files

---
+++ IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/SignupPageActivity.java
@@ -0,0 +1,17 @@
+package edu.oregonstate.studentlife.ihcv2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Omeed on 12/20/17.
+ */
+
+public class SignupPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
