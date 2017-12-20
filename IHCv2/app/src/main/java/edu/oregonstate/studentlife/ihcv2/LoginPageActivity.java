ef6ebb9fd0100ef85cfbb8f8d32b9d829c1004f3
blazerzero <habibelo@oregonstate.edu>
2017-12-20 14:35:08 -0800

    Login and logout stuff --> Loginpage and logoutpage

--- IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/LoginPageActivity.java
+++ IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/LoginPageActivity.java
@@ -1,15 +1,17 @@
-/* WELCOME SCREEN W/ LOG IN/SIGN UP BUTTONS */
-
 package edu.oregonstate.studentlife.ihcv2;
+
+
+/**
+ * Created by dylan on 12/20/2017.
+ */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LogoutActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logout);
+        setContentView(R.layout.activity_loginpage);
     }
 }
--- IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/LogoutPageActivity.java
+++ IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/LogoutPageActivity.java
@@ -1,17 +1,15 @@
+/* WELCOME SCREEN W/ LOG IN/SIGN UP BUTTONS */
+
 package edu.oregonstate.studentlife.ihcv2;
-
-
-/**
- * Created by dylan on 12/20/2017.
- */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LogoutActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_logoutpage);
     }
 }
