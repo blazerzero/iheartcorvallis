c7e24a843f155792a726d734b666a35566c4abca
Bradley Imai <imaib@onid.oregonstate.edu>
2017-12-20 14:56:12 -0800

    loginpage stuff

--- IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/LoginPageActivity.java
+++ IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/LoginPageActivity.java
@@ -2,6 +2,9 @@ package edu.oregonstate.studentlife.ihcv2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Omeed on 12/20/17.
@@ -13,5 +16,23 @@ public class LoginPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
+
+            Button searchButton = (Button)findViewById(R.id.btn_search);
+            searchButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginPageActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            Button viewButton = (Button)findViewById(R.id.btn_view);
+            viewButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginPageActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
