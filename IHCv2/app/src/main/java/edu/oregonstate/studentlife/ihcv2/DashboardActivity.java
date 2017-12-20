83ad106bb905e383e873795060bb00aa63eeac35
Bradley Imai <imaib@onid.oregonstate.edu>
2017-12-20 14:55:16 -0800

    login page template

--- IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/DashboardActivity.java
+++ IHCv2/app/src/main/java/edu/oregonstate/studentlife/ihcv2/DashboardActivity.java
@@ -2,6 +2,9 @@ package edu.oregonstate.studentlife.ihcv2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Omeed on 12/20/17.
@@ -12,5 +15,6 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        }
     }
-}
