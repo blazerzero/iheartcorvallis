c67bd5f6dc99b92277e2e90c30b0109b5fcd246b
blazerzero <habibelo@oregonstate.edu>
2018-02-07 16:04:35 -0800

    Reorganized ihc_server directory. links in discord to the pages have changed.

--- ihc_server/add_event.php
+++ ihc_server/add_event.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-   <link type="text/css" rel="stylesheet" href="./Semantic-UI-CSS-master/semantic.css"/>
-   <link type="text/css" rel="stylesheet" href="./stylesheet.css"/>
+   <link type="text/css" rel="stylesheet" href="./css/Semantic-UI-CSS-master/semantic.css"/>
+   <link type="text/css" rel="stylesheet" href="./css/stylesheet.css"/>
    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
    <script>
    $(document).ready(function() {
@@ -77,40 +77,39 @@
    </ul>
 
    <div class="mainbody">
-      <left class="sectionheader"><h1>Add an Event</h1></left>
+      <left class="sectionheader">Add an Event</left>
       <br>
-      <strong class="requirednote">* Denotes a required field</strong><br><br>
-
-      <form name="eventForm" onsubmit="return validateForm()" action="add_events_server.php" method="post">
+      <br><p class="requirednote">* Denotes a required field</p><br>
+      <form name="eventForm" onsubmit="return validateForm()" action="../admin_server/add_events_server.php" method="post">
          <div class="elem">
-            *Name of Event: <input class="inputbox" type="text" name="name"><br><br>
+            <span class="requirednote">*</span>Name of Event: <input class="inputbox" type="text" name="name"><br><br>
          </div>
          <div class="elem">
-            *Name of Location: <input class="inputbox" type="text" name="location"><br><br>
+            <span class="requirednote">*</span>Name of Location: <input class="inputbox" type="text" name="location"><br><br>
          </div>
          <div class="elem">
-            *Street Address: <input class="inputbox" type="text" name="streetaddress"><br>
+            <span class="requirednote">*</span>Street Address: <input class="inputbox" type="text" name="streetaddress"><br>
          </div>
          <div class="elem">
-            *City: <input class="inputbox" type="text" name="city"><br>
+            <span class="requirednote">*</span>City: <input class="inputbox" type="text" name="city"><br>
          </div>
          <div class="elem">
-            *State: <input class="inputbox" type="text" name="state"><br>
+            <span class="requirednote">*</span>State: <input class="inputbox" type="text" name="state"><br>
          </div>
          <div class="elem">
-            *ZIP: <input class="inputbox" type="text" name="zip"><br><br>
+            <span class="requirednote">*</span>ZIP: <input class="inputbox" type="text" name="zip"><br><br>
          </div>
          <div class="elem">
-            *Date: <input class="inputbox" type="date" name="date"><br><br>
+            <span class="requirednote">*</span>Date: <input class="inputbox" type="date" name="date"><br><br>
          </div>
          <div class="elem">
-            *Time: <input class="inputbox" type="time" name="time"><br><br>
+            <span class="requirednote">*</span>Time: <input class="inputbox" type="time" name="time"><br><br>
          </div>
          <div class="elem">
-            *Description of Event: <input class="inputbox" type="text" name="description"><br><br>
+            <span class="requirednote">*</span>Description of Event: <input class="inputbox" type="text" name="description"><br><br>
          </div>
          <div class="elem">
-            *Cover Image: <input class="ui button" type="file" name="image"><br><br>
+            <span class="requirednote">*</span>Cover Image: <input class="ui button" type="file" name="image"><br><br>
          </div>
          <div class="elem">
             Link 1: <input class="inputbox" type="text" name="link1"><br><br>
