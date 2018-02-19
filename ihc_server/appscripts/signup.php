6768d60f248424fc3edb409db0e9a4c430f408e3
dygeto <tomlinsd@oregonstate.edu>
2018-02-19 15:08:39 -0800

    testing first prepared statement in signup script

--- ihc_server/appscripts/signup.php
+++ ihc_server/appscripts/signup.php
@@ -31,9 +31,15 @@
       if ($alreadyExists == False) {
          $result = $mysqli->query("SELECT id FROM ihc_users");
          $id = $result->num_rows + 1;
-         $result = $mysqli->query("INSERT INTO ihc_users (student, firstname, lastname, email, password, id) VALUES ('$student', '$firstname', '$lastname', '$email', '$password', '$id')");
+		 
+		 $stmt = $mysqli->prepare("INSERT INTO ihc_users (student, firstname, lastname, email, password, id) VALUES (?, ?, ?, ?, ?, ?)");
+         $stmt->bind_param($student, $firstname, $lastname, $email, $password, $id);
+		 
+		 $stmt->execute();
+		 
+		// $result = $mysqli->query("INSERT INTO ihc_users (student, firstname, lastname, email, password, id) VALUES ('$student', '$firstname', '$lastname', '$email', '$password', '$id')");
 
-         if ($result == True) {
+         if ($stmt == True) {
             echo "SIGNUPSUCCESS"; # account successfully added to database
          }
          else {
