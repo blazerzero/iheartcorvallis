f8ddc73893b7b8f6915ada7563ae2eae42927f85
dygeto <tomlinsd@oregonstate.edu>
2018-03-05 15:30:44 -0800

    added PBKDF2 hashing validation for admin

--- ihc_server/admin_server/login.php
+++ ihc_server/admin_server/login.php
@@ -29,13 +29,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $email = $_POST["email"];
    $password = $_POST["password"];
 
-   $result = $mysqli->query("SELECT email FROM ihc_admin_users WHERE email='$email' AND password='$password'");
+   $result = $mysqli->query("SELECT password FROM ihc_admin_users WHERE email='$email'");
    if ($result->num_rows > 0) {
-      $_SESSION["id"] = $email;
-      //$sessionid = $_SESSION["id"];
-      //session_regenerate_id(true);
-      /*$user = $result->fetch_assoc();
-      if ($email == $user["email"]) {*/
+	   
+	   $row = $result->fetch_assoc();
+	   
+	   // user's stored password that we must compare with
+	   $pHash = $row['password'];
+	   
+	   $iterations = 1000;
+	   
+	   // use this to salt the original password
+	   //$salt = openssl_random_pseudo_bytes(16);
+	   
+	   // need to get the salt from the hash
+	   $storedpHash = explode("|", $pHash);// salt of stored password
+	   
+	   $hash = hash_pbkdf2("sha256",$password, $storedpHash[0], $iterations, 50);
+	   
+	   if (strcmp($hash,$storedpHash[1])) {
+	     // logic after checking hash password
+		 $_SESSION["id"] = $email;
+	  
+	  
+         //$sessionid = $_SESSION["id"];
+         //session_regenerate_id(true);
+         /*$user = $result->fetch_assoc();
+         if ($email == $user["email"]) {*/
          //$message = "Correct login credentials!"; # correct login credentials
          //echo "<script type='text/javascript'>alert('$message');</script>";
          $url = "../index.php";
@@ -43,8 +63,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
          /*echo $_SESSION["id"]."<br>";
          $id = checkAuth();
          echo "ID: ".$id."<br>";*/
-      //}
-
+         //}
+	   }
       //$message = "Incorrect login credentials!"; # error logging in
 
    }
